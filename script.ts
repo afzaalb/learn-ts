c630aeebd55fa70781aff8ae6bca265e92d8f647
Afzaal B <afzaalopera@gmail.com>
2019-09-22 13:19:36 +0500

    Basic compilation of TS files to JS and view in browser

---
+++ script.ts
@@ -0,0 +1,10 @@
+const hello = "Hello";
+
+const world = "World";
+
+console.log(hello + " " + world);
+
+// After adding this file we run the command in terminal
+// Command is tsc <filename.ts>
+// This will compile <filename.ts> to <filename.js>
+// which we can seen working in browser
